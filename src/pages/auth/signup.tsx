5e8c5cf892e181419750b6dbdda03851a6019df0
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2025-11-17 18:11:49 +0100

    update

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -361,7 +361,7 @@ export function SignUp() {
 
         <CardContent>
           <Tabs defaultValue="owner" className="w-full">
-            <TabsList className="grid w-full grid-cols-2">
+            <TabsList className="grid w-full grid-cols-1">
               <TabsTrigger value="owner">Restaurant Registration</TabsTrigger>
             </TabsList>
 
