370f426a67b27cc8dbc676891cfc88bdcdc4a7b7
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2026-06-18 17:49:54 +0100

    Added Vercel.json - Fixes 404 on refresh

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import * as path from "node:path";
 
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
